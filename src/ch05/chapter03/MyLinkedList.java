7b577c13922b689ee5a6669afb88c55ad62c748b
stillgood96 <a787574@naver.com>
2021-11-09 23:50:23 +0900

    CH05-03 LinkedList 수정

--- src/ch05/chapter03/MyLinkedList.java
+++ src/ch05/chapter03/MyLinkedList.java
@@ -15,27 +15,36 @@ public class MyLinkedList {
     {
 
         MyListNode newNode;
-        if(head == null){  //맨 처음일때
+        if(head == null){  //맨 처음일때 - head가 null이라면 첫 노드라는 뜻
             newNode = new MyListNode(data);
             head = newNode;
         }
         else{
             newNode = new MyListNode(data);
             MyListNode temp = head;
-            while(temp.next != null)  //맨 뒤로 가서
+            while(temp.next != null) {
                 temp = temp.next;
+
+                // temp.next가 null이 아닐동안 계속 뒤로 넘어가는 것 .
+                // temp null이 아니면 temp는 temp의 nextNode를 가르키게되니까
+                // temp.next가 null 즉 마지막까지 순회해서  nextNode를 가르키게 될것
+            }
+            // 그렇게 마지막까지 순회한 Node의 nextNode 값에 추가할 node 값을 넣어주는 것이다.
             temp.next = newNode;
         }
         count++;
         return newNode;
     }
 
+    // 원하는 위치에 node를 집어 넣기
     public MyListNode insertElement(int position, String data )
     {
         int i;
         MyListNode tempNode = head;
         MyListNode newNode = new MyListNode(data);
 
+
+        // 원하는 위치가 0보다 작거나 생성된 LinkedList 보다 큰 index에 연결하려고 하거나..
         if(position < 0 || position > count ){
             System.out.println("추가 할 위치 오류 입니다. 현재 리스트의 개수는 " + count +"개 입니다.");
             return null;
--- src/ch05/chapter03/MyListNode.java
+++ src/ch05/chapter03/MyListNode.java
@@ -5,16 +5,19 @@ public class MyListNode {
     private String data;       // 자료
     public MyListNode next;    // 다음 노드를 가리키는 링크
 
+    // 기본생성자를 이용했을땐 data도 next도 null이 들어간다.
     public MyListNode(){
         data = null;
         next = null;
     }
 
+    // data(자료)를 받는 생성자도 생성해준다.
     public MyListNode(String data){
         this.data = data;
         this.next = null;
     }
 
+    // data(자료), link를 받는 생성자도 생성해준다. 수정을 위한 ?
     public MyListNode(String data, MyListNode link){
         this.data = data;
         this.next = link;
